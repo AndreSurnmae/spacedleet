a3b2586bd11f9caa0af252011f281e2621c29098
AndreSurnam <tolentino.andre.edups@gmail.com>
2025-04-16 21:37:50 -0400

    Change anchor tag to Link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <footer className="flex flex-row items-center justify-between md:px-5 py-4 bg-white text-black">
             <div className="flex items-center md:ml-[85px] ml-[25px] text-[12px]">
-                <img 
-                    src="/SpacedLeetLogo_bh.svg"
+                <Image 
+                    src={"/SpacedLeetLogo_bh.svg"}
                     alt="Logo"
+                    width={25}
+                    height={25}
                     className="w-[25px]"
                 />
                 <div className="flex items-center gap-2 ml-[10px] text-[12px]">
@@ -20,11 +25,11 @@ export default function Footer() {
             </div>
 
             <div className="flex mr-[25px] md:mr-[85px] gap-6 text-[12px]">
-                <a href="/" className="hover:text-[#4973bf] hover:underline">About</a>
-                <a href="/" className="hover:text-[#4973bf] hover:underline">Accessibility</a>        
-                <a href="/signup" className="hover:text-[#4973bf] hover:underline">Join Us</a>
-                <a href="/" className="hover:text-[#4973bf] hover:underline">Privacy Policy</a>
-                <a href="/" className="hover:text-[#4973bf] hover:underline">User Agreement</a>
+                <Link href="/" className="hover:text-[#4973bf] hover:underline">About</Link >
+                <Link href="/" className="hover:text-[#4973bf] hover:underline">Accessibility</Link >        
+                <Link href="/signup" className="hover:text-[#4973bf] hover:underline">Join Us</Link >
+                <Link href="/" className="hover:text-[#4973bf] hover:underline">Privacy Policy</Link >
+                <Link href="/" className="hover:text-[#4973bf] hover:underline">User Agreement</Link >
             </div>
         </footer>
     );
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,28 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
     return (
       <nav className="flex items-center justify-between md:px-5 py-4 shadow-md bg-white text-black">
-        <a href="/" className="flex items-center gap-2 font-bold text-xl md:ml-[85px] ml-[25px]">
-          <img 
-            src="/SpacedLeetLogo_bh.svg"
+        <Link href="/" className="flex items-center gap-2 font-bold text-xl md:ml-[85px] ml-[25px]">
+          <Image 
+            src={"/SpacedLeetLogo_bh.svg"}
             alt="Logo"
+            width={40}
+            height={40}
             className="w-15"
+
           />
           <p className="md:block hidden">
             <span className="text-">Spaced</span>
             <span className="text-[#4973bf]">Leet</span>
           </p>
-        </a>
+        </Link>
           <div className="flex items-center gap-6 font-light md:mr-[85px] mr-[25px]">
-          <a 
+          <Link 
             href="/login" 
             className="text-sm hover:text-blue-500">
             Log in
-          </a>
-          <a
+          </Link>
+          <Link
             href="/signup"
             className="text-sm px-6 py-3 border-[2px] border-[#4973bf] rounded-[15px] hover:bg-[#5a83db] hover:text-white transition">
             Sign up
-          </a>
+          </Link>
         </div>
       </nav>
     );
